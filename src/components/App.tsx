14ee5d16bf7bb2ae4b42864566642118ddded80c
mastercodercat <smartgeek.taylor@gmail.com>
2022-05-03 11:58:19 -0700

    Initialized repository with react and vite

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="bg-white">
+      <div className="py-16 px-4 mx-auto max-w-screen-xl sm:py-24 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h2 className="text-base font-semibold tracking-wide text-blue-600 uppercase">
+            CanTo DEX
+          </h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
